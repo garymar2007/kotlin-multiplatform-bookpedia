8962fcbce5fe0f0aea67137d02c4d750c4513fd7
garyma <garymar2007@gmail.com>
2025-01-04 23:18:04 +0200

    finished teh booksearch bar.

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Preview.kt
@@ -1,10 +1,13 @@
 package com.plcoding.bookpedia
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
@@ -18,4 +21,32 @@ private fun BookSearchBarPreview() {
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+private val books = (1 .. 100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        authors = listOf("Gary Ma"),
+        imageUrl = "https://picsum.photos/200/300?random=$it",
+        description = "This is book $it",
+        languages = emptyList(),
+        firstPublishYear = "2023",
+        averageRating = 4.5,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 3
+
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -4,11 +4,12 @@ data class Book(
     val id: String,
     val title: String,
     val imageUrl: String,
-    val author: List<String>,
+    val authors: List<String>,
     val description: String,
-    val language: List<String>,
+    val languages: List<String>,
     val firstPublishYear: String?,
     val averageRating: Double?,
     val ratingCount: Int?,
+    val numPages: Int?,
     val numEditions: Int
 )
